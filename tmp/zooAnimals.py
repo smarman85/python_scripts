204219821a92897ffa6e4d6d607a0b7956fa62bc
vagrant <vagrant@localhost.localdomain>
2017-09-18 23:31:36 -0700

    Zoo animals

--- tmp/zooAnimals.py
+++ tmp/zooAnimals.py
@@ -1,8 +1,14 @@
 class Animal(object):
     legs = 2
-    isAlive = true
+    isAlive = True
 
     def __init__(self, specie, age, habitat):
         self.specie  = specie
         self.age     = age
         self.habitat = habitat
+
+    def describe(self):
+      return "Hi I am a %s and I live in the %s. I am %d years old. I have %s legs." % (self.specie, self.habitat, self.age, self.legs)
+
+jerry = Animal("Kangaroo", 21, "outback")
+print jerry.describe()
